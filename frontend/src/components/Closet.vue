7a82c92a834b7d26a3869ff9e8f0ec378b607e86
Christina Sun <csun@bayviewglen.ca>
2025-05-27 19:37:15 -0400

    archive feature done, except archived modal

--- frontend/src/components/Closet.vue
+++ frontend/src/components/Closet.vue
@@ -9,6 +9,13 @@
         :items="types.map(type => type.name)"
       ></v-select>  
       <v-btn @click="clearFilters()">Clear Filters</v-btn>
+      <v-btn 
+        color="error" 
+        class="ml-2" 
+        :disabled="selectedImageIds.length == 0"
+        @click="archiveSelectedImages()">
+        Archieve Selected 
+      </v-btn>
     </div>
     <!-- file input -->
     <v-row justify="center" class="mb-6">
@@ -42,8 +49,16 @@
 
     <!-- images -->
     <v-row>
-      <v-col v-for="(image, index) in filteredImages" :key="index" cols="12" md="4" sm="6" lg="3">
-        <v-card>
+      <v-col 
+        v-for="(image, index) in filteredImages" 
+        :key="index" 
+        cols="12" 
+        md="4" 
+        sm="6" 
+        lg="3"
+        @click="toggleSelectImage(image._id)"
+      >
+        <v-card class="image-card" :class="{'selected-image': selectedImageIds.includes(image._id)}">
           <v-img :src="image.url" aspect-ratio="1" class="white--text align-end"></v-img>
         </v-card>
       </v-col>
@@ -81,6 +96,7 @@ export default {
         { name: 'hat' },
         { name: 'accessory' },
       ],
+      selectedImageIds: [],
     };
   },
   mounted() {
@@ -90,7 +106,7 @@ export default {
     // watch types filter
     'filterTypes': function(newValue, oldValue) {
       if (newValue == ''){
-        this.filteredImages = this.images
+        this.applyFilter()
       } else {
         this.applyFilter()
       }
@@ -101,7 +117,7 @@ export default {
       try {
         const images = await axios.get(`https://closet-backend-huo7.onrender.com/api/images`); // change link to whatever it is
         this.images = images.data
-        this.filteredImages = images.data
+        this.applyFilter()
         // images.data.forEach(image => {
         //   if(image.type == "top") {
         //     this.tops.push(image)
@@ -120,14 +136,19 @@ export default {
       }
     },
     applyFilter(){
-      this.filteredImages = this.images.filter(image => {
-        const imageType = image.type
-        return imageType == this.filterTypes
-      })
+      let filtered = this.images.filter(image => !image.archived)
+      if (this.filterTypes && this.filterTypes.length > 0) {
+        filtered = filtered.filter(image => this.filterTypes.includes(image.type));
+      }
+      this.filteredImages = filtered;
+      this.selectedImageIds = this.selectedImageIds.filter(id => 
+        this.filteredImages.some(image => image._id === id)
+      );
     },
     clearFilters(){
       this.filterTypes = []
-    },
+      this.applyFilter()
+  },
     async uploadImage() {
       if (!this.selectedImages.length) {
         alert('Please select an image to upload.');
@@ -175,10 +196,43 @@ export default {
         this.snackbar = true;
       }
     },
+    toggleSelectImage(imageId) {
+      console.log(this.selectedImageIds)
+      const index = this.selectedImageIds.indexOf(imageId);
+      if (index == -1) {
+        this.selectedImageIds.push(imageId);
+      } else {
+        this.selectedImageIds.splice(index, 1);
+      }
+    },
+    async archiveSelectedImages(){
+      try {
+        await Promise.all(this.selectedImageIds.map(id => {
+          axios.put(`http://localhost:5000/api/images/archive/${id}`, { archived: true });
+        }));
+        this.snackbarText = 'Selected images archived successfully!',
+        this.snackbar = true,
+        this.selectedImageIds = [];
+        this.getImages();
+      } catch (err) {
+        console.error('Failed to archive selected images:', err);
+        this.snackbarText = 'Failed to archive selected images. Please try again.';
+        this.snackbar = true;
+      }
+    }
   },
 }
 </script>
   
   
-<style>
+<style scoped>
+.image-card {
+  cursor: pointer;
+  transition: box-shadow 0.2s, border 0.2s;
+}
+.selected-image {
+  border: 3px solid #1976d2 !important;
+  box-shadow: 0 0 10px #1976d2;
+}
+
 </style>
