e38afe831490ab46785e91c4ef3cb197189a748e
Christina Sun <csun@bayviewglen.ca>
2025-07-21 21:00:08 -0400

    fixing cors policy

--- api/server.js
+++ api/server.js
@@ -16,7 +16,7 @@ const app = express()
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', // frontend URL
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173', // frontend URL
   credentials: true
 }))
 app.use(session({
